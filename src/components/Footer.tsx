90c97ad4244a9b9d3ad0610f03ef2f42827ea836
Bladesheng <keadr23@gmail.com>
2022-09-24 09:37:07 +0200

    Initial setup

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Made by <a href="https://github.com/Bladesheng">Bladesheng</a>
+      </p>
+    </footer>
+  );
+}
